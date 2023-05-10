be2c7361a86e79bb3173b1ebaddbd209dc18ef23
richyrichyrich38 <85546279+richyrichyrich38@users.noreply.github.com>
2023-05-10 08:12:49 +0100

    cannon-es added

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as CANNON from 'cannon-es';
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
